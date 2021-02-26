12b347f9d6c27990776fc6c8c8ba490e029a7f33
tvittitoe <tvittitoe@gmail.com>
2021-02-26 07:20:15 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-# Disable screen blanking
+## Move Toolbar
+sudo cp /etc/xdg/lxpanel/LXDE-pi/panels/panel ~/.config/lxpanel/LXDE-pi/panels/ 
+sudo sed -i 's/edge=top/edge=bottom/' ~/.config/lxpanel/LXDE-pi/panels/panel
 
+## Update Hostname
+read -p "New Hostname: " replace
+echo replace | sudo tee -a /etc/hostname
+echo '127.0.1.1 ' $replace | sudo tee -a /etc/hosts
+
+## Force Hotplug
+
+## Set Resolution
+
+## Disable screen blanking
+sudo sed -i 's/$/ consoleblank=0/' /boot/cmdline.txt
+
+
+## Set Background
 
 # Enable SSH
 sudo systemctl enable ssh
@@ -17,15 +33,18 @@ sudo npm i -g pm2 socket.io
 sudo systemctl enable vncserver-x11-serviced.service
 sudo systemctl start vncserver-x11-serviced.service
 
-# Create pm2 instance to run on startup and monitor script
+## Create pm2 instance to run on startup and monitor script
 sudo pm2 start /home/pi/fieldboard/fboard.js --watch
 sudo env PATH=$PATH:/usr/local/bin pm2 startup -u pi
 
-# Set up ZeroTier
+## Set up ZeroTier
 curl -s https://install.zerotier.com | sudo bash
 sudo zerotier-cli join 3efa5cb78a8c50a6
 
-# Todo
-# Watch for css or html update, Chrome Watch, Initialize to refresh?, USB Tunneling, ead-only, html/java cases, Set Resolution,
-#Fix screen blanking, set background, move toolbar
+## USB Tunneling
+
+## Chrom Autorun
+
+## Read-Only
+
 
