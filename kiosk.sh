f42cc8aa230abd11cc0335ac1fd6fe50fcfba6b7
tvittitoe <tvittitoe@gmail.com>
2021-03-02 22:45:33 -0500

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -5,4 +5,4 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://127.0.0.1:8000
+/usr/bin/chromium --noerrdialogs --disable-infobars --kiosk http://127.0.0.1:8000
